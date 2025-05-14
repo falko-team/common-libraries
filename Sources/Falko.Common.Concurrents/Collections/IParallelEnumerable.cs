f7f072b61b75775175b426a7ce3c8084e8733c7a
Rima Falko <fembina@pm.me>
2025-05-14 03:50:10 +0200

    add parallel-enumerable and parallel-enumerator interfaces and implement task extension methods

---
+++ Sources/Falko.Common.Concurrents/Collections/IParallelEnumerable.cs
@@ -0,0 +1,14 @@
+namespace Falko.Concurrents.Collections;
+
+/// <summary>
+/// Represents a parallel enumerable. This enumerable can be enumerated in parallel.
+/// </summary>
+/// <typeparam name="T">The type of the elements in the collection.</typeparam>
+public interface IParallelEnumerable<T>
+{
+    /// <summary>
+    /// Creates a parallel enumerator for the collection.
+    /// </summary>
+    /// <returns>A parallel enumerator that can be used to enumerate the collection in parallel.</returns>
+    IParallelEnumerator<T> GetParallelEnumerator();
+}
